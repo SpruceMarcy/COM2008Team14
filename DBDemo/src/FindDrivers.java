0a629b23a27b10e84d741cdeb7bf77d15f2c2c67
lailing2000 <34120661+lailing2000@users.noreply.github.com>
2019-11-14 16:00:34 +0000

    find drivers demo added

---
+++ DBDemo/src/FindDrivers.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+import java.util.*;
+public class FindDrivers {
+	public static void main(String[] args) throws Exception {
+	System.out.println("\nDrivers loaded as properties:");
+	System.out.println(System.getProperty("jdbc.drivers"));
+	System.out.println("\nDrivers loaded by DriverManager:");
+	Enumeration<Driver> list = DriverManager.getDrivers();
+	while (list.hasMoreElements())
+		System.out.println(list.nextElement());
+	String url = "jdbc:mysql://stusql.dcs.shef.ac.uk/team014";
+	String user = "team014";
+	String password = "80019d16";
+	try(Connection con = DriverManager.getConnection(url, user, password)){
+		System.out.print("connected?");
+		Statement stmt = con.createStatement();
+		ResultSet res = stmt.executeQuery("SELECT * FROM INFORMATION_SCHEMA.TABLES");
+		System.out.println(res);
+		while(res.next()) {
+			String txt = res.getString(1);
+			System.out.println(txt);
+		}
+		res.close();
+	}
+	
+	}
+}
